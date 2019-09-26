39437b5bfbe261ac49953afa2b40eeeeb64e1f86
Pavel Baranov <bestamvo@gmail.com>
2019-09-26 23:38:33 +0400

    First

---
+++ app/src/main/java/com/example/cuckoobye/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cuckoobye
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
